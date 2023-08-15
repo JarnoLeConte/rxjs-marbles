a2a3b623ab0e03e2ba464f08dfa9fd020205d9cd
Jarno Le Conté <jarno.leconte@me.com>
2023-08-16 00:16:43 +0200

    Simplify logic by using `useImperativeHandle`

--- app/components/Build.tsx
+++ app/components/Build.tsx
@@ -1,26 +1,23 @@
 import type { ForwardedRef } from "react";
-import { forwardRef, useImperativeHandle } from "react";
+import { forwardRef } from "react";
 import type { ObservableBuilder, OperatorBuilder } from "~/types";
 import type { TrackHead, TrackTail } from "./track/parts";
 import { Part } from "./track/parts";
+import { Empty } from "./track/reactive-track/Empty";
+import { Identity } from "./track/reactive-track/Identity";
 import { Map } from "./track/reactive-track/Map";
 import { Producer } from "./track/reactive-track/Producer";
 import { Ramp } from "./track/reactive-track/Ramp";
-import { EMPTY, pipe } from "rxjs";
 
 export const Build = forwardRef(function Build(
   { track }: { track: TrackHead },
   ref: ForwardedRef<ObservableBuilder>
 ) {
-  useImperativeHandle(ref, () => ({ build: () => EMPTY }), []);
-
-  if (!track) return null;
-
   switch (track.part) {
     case Part.Producer:
       return <Producer ref={ref} track={track} />;
     default:
-      return null;
+      return <Empty ref={ref} />;
   }
 });
 
@@ -28,16 +25,12 @@ export const BuildTail = forwardRef(function BuildTail(
   { track }: { track: TrackTail },
   ref: ForwardedRef<OperatorBuilder>
 ) {
-  useImperativeHandle(ref, () => ({ build: () => pipe() }), []);
-
-  if (!track) return null;
-
-  switch (track.part) {
+  switch (track?.part) {
     case Part.Ramp:
       return <Ramp ref={ref} track={track} />;
     case Part.Map:
       return <Map ref={ref} track={track} />;
     default:
-      return null;
+      return <Identity ref={ref} />;
   }
 });
---
+++ app/components/track/reactive-track/Identity.tsx
@@ -0,0 +1,21 @@
+import type { ForwardedRef } from "react";
+import { forwardRef, useImperativeHandle } from "react";
+import { pipe } from "rxjs";
+import type { OperatorBuilder } from "~/types";
+
+export const Identity = forwardRef(function Identity(
+  props,
+  ref: ForwardedRef<OperatorBuilder>
+) {
+  useImperativeHandle(
+    ref,
+    () => ({
+      build() {
+        return pipe();
+      },
+    }),
+    []
+  );
+
+  return null;
+});
