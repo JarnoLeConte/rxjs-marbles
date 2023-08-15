8ca4a56035c78b534dde658bfa8f2723a49e42e0
Jarno Le Conté <jarno.leconte@me.com>
2023-08-15 21:34:08 +0200

    Show test scene in development mode

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -5,7 +5,10 @@ export function Menu() {
   return (
     <div>
       {examples
-        .filter(({ name }) => name !== "test")
+        .filter(
+          ({ name }) =>
+            name !== "test" || process.env.NODE_ENV === "development"
+        )
         .map(({ name }) => (
           <Link key={name} to={`/examples/${name}`} className="menu-item">
             {name}
